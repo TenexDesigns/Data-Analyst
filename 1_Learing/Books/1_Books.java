08c8351508051378f7741b26685dd88076da3045
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-08 15:57:38 +0300

    Create 1_Books.java

---
+++ 1_Learing/Books/1_Books.java
@@ -0,0 +1,59 @@
+Here are some of the best books to read and become skilled in data analytics and its related areas:
+
+**Mathematics**
+
+* **Statistics:**
+    * **Statistics in Plain English** by Timothy C. Urdan
+    * **Naked Statistics** by Charles Wheelan
+    * **The Book of Numbers** by John A. Paulos
+
+* **Probability Theory:**
+    * **Probability and Statistics** by Morris H. DeGroot and Casella
+    * **An Introduction to Probability Theory and Statistical Inference** by Frank Yates
+    * **Probability Theory** by Sheldon M. Ross
+
+* **Linear Algebra:**
+    * **Introduction to Linear Algebra** by Gilbert Strang
+    * **Linear Algebra Done Right** by Sheldon M. Ross
+    * **Linear Algebra and Its Applications** by Gilbert Strang
+
+**Programming Languages**
+
+* **Python:**
+    * **Python for Data Analysis** by Wes McKinney
+    * **Learning Python** by Mark Lutz
+    * **Automate the Boring Stuff with Python** by Al Sweigart
+
+* **R:**
+    * **R for Data Science** by Garrett Grolemund and Hadley Wickham
+    * **The R Revolution** by Hadley Wickham and Garrett Grolemund
+    * **An Introduction to R** by Deepayan Sarkar
+
+* **SQL:**
+    * **SQL in 10 Minutes, Sams Teach Yourself** by Ben Forta
+    * **Head First SQL** by Lynn Beighley
+    * **SQL: A Beginner's Guide** by Greg D. Squires
+
+**Data Analysis Tools**
+
+* **Tableau:**
+    * **Tableau Your Data!: Interactive Data Visualization Analysis** by Daniel Roth
+    * **Learning Tableau 2020** by Joshua Ball
+    * **Tableau Desktop Quick Start Guide** by Tableau
+
+* **Power BI:**
+    * **Data Analysis with Microsoft Power BI** by Alberto Ferrari and Marco Russo
+    * **Power BI and Data Analytics: A Practical Guide** by Colin Ware
+    * **Power BI for Beginners** by Matt Allsopp
+
+* **Qlik Sense:**
+    * **Qlik Sense Business Intelligence Cookbook** by David Smith
+    * **Qlik Sense 3.1: A Complete Guide to Data Visualization, Analytics, and Self-Service BI** by Denis Cousineau
+    * **Qlik Sense: The Beginner's Guide** by Paul Harrison
+
+This list provides a starting point for your learning journey. As you progress, you may find other books or resources that are more relevant to your specific interests and goals.
+
+
+
+
+  ...
