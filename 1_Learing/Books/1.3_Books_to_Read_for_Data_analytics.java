33af06b033e2debc53e23225cebfc05edd10eb9c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-08 16:02:05 +0300

    Create 1.3_Books_to_Read_for_Data_analytics.java

---
+++ 1_Learing/Books/1.3_Books_to_Read_for_Data_analytics.java
@@ -0,0 +1,28 @@
+Here are some of the best books to read to become skilled in data analytics:
+
+* **Data Science for Business** by Foster Provost and Tom Fawcett: This book provides a comprehensive introduction to data science for business professionals. It covers the basics of data mining, data analysis, and machine learning, as well as how to apply these techniques to solve real-world business problems.
+
+* **Naked Statistics: Stripping the Covers Off Numbers** by Charles Wheelan: This book is a great introduction to statistics for non-statisticians. It explains statistical concepts in a clear and concise way, using everyday examples to make them relatable.
+
+* **An Introduction to Statistical Learning** by Gareth James, Daniela Witten, Trevor Hastie, and Robert Tibshirani: This book is a classic textbook on statistical learning. It covers a wide range of topics, including linear regression, logistic regression, decision trees, and support vector machines.
+
+* **Python for Data Analysis** by Wes McKinney: This book is a hands-on introduction to Python for data analysis. It covers the basics of Python programming, as well as how to use Python libraries like NumPy, Pandas, and Matplotlib for data manipulation, analysis, and visualization.
+
+* **R for Data Science** by Garrett Grolemund and Hadley Wickham: This book is a hands-on introduction to R for data science. It covers the basics of R programming, as well as how to use R packages like dplyr, ggplot2, and tidyverse for data manipulation, analysis, and visualization.
+
+* **SQL in 10 Minutes, Sams Teach Yourself** by Ben Forta: This book is a quick and easy introduction to SQL. It covers the basics of SQL syntax and how to use SQL to query and manipulate data in relational databases.
+
+* **Learning Tableau 2020** by Joshua Ball: This book is a hands-on introduction to Tableau. It covers the basics of Tableau, as well as how to use Tableau to create interactive data visualizations.
+
+* **Mastering Machine Learning with Python** by Dipanjan Sarkar: This book covers a wide range of machine learning topics, including supervised learning, unsupervised learning, and reinforcement learning. It also includes code examples and exercises to help you learn the material.
+
+* **Deep Learning** by Ian Goodfellow, Yoshua Bengio, and Aaron Courville: This book is a comprehensive textbook on deep learning. It covers the basics of deep learning, as well as advanced topics like convolutional neural networks and recurrent neural networks.
+
+* **Artificial Intelligence: A Modern Approach** by Stuart Russell and Peter Norvig: This book is a classic textbook on artificial intelligence. It covers a wide range of AI topics, including search, reasoning, planning, and machine learning.
+
+These are just a few of the many great books that can help you become skilled in data analytics. With hard work and dedication, you can learn the skills you need to succeed in this exciting field.
+
+
+
+
+  ....
