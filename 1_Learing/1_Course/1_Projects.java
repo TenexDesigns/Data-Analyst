76f8ac7b06ae52e3fa2a10d27d03aea8a7041034
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-10 14:04:10 +0300

    Create 1_Projects.java

---
+++ 1_Learing/1_Course/1_Projects.java
@@ -0,0 +1,28 @@
+Projects and Data Sets:
+Data Integration Project:
+
+Design and implement a solution to integrate data collection systems and automate data flow.
+Dashboard Creation Project:
+
+Create rich interactive visualizations using real-world datasets.
+Market Analysis Project:
+
+Conduct a market analysis using media metrics, KPIs, and visualization tools.
+Advanced Analytics Project:
+
+Develop and apply advanced analytics techniques to solve a specific business problem.
+
+
+  Books and Courses:
+"The Data Warehouse Toolkit" by Ralph Kimball:
+
+Comprehensive guide to data warehouse design.
+Online Courses:
+
+Platforms like Coursera, edX, and Udacity offer courses in SQL, data analysis, and visualization.
+"Data Science for Business" by Foster Provost and Tom Fawcett:
+
+Understand the intersection of data science and business.
+Documentation and Best Practices:
+
+Explore documentation and best practices related to SQL Server, SSAS, and data analysis tools.
