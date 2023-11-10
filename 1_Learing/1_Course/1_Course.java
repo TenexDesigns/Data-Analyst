c9c5a1a14f2d349bea120ba6fa3228a5c78bfac4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-10 13:56:53 +0300

    Create 1_Course.java

---
+++ 1_Learing/1_Course/1_Course.java
@@ -0,0 +1,94 @@
+I found this course outline, is it good, can I follw it , is there any more modules or btools or skills you can add to it "1. IIM SKILLS
+IIM SKILLS is the most highly rated ed-tech company in the world. Their data analytics course is one of their highest recommened course.
+
+Modules- Data Analytics Course with IIM SKILLS
+Module Name	Important Topic
+Module 1: Basic and Advance Excel
+  
+              Introduction to Data Handling
+              Data Manipulation Using Functions
+              Data Analysis and Reporting
+              Data Visualization in Excel
+              Overview of Dashboards
+
+  
+Module 2: Visual Basic Application
+  
+              Introducing VBA
+              How VBA Works with Excel
+              Key Components of Programming Language
+              Programming Constructs in VBA
+              Functions & Procedures in VBA
+              Objects & Memory Management in VBA
+              Error Handling
+              Controlling Accessibility of Your Code
+              Communicating with Your Users
+Module 3: SQL
+              Basics RDBMS Concepts
+              Utilizing the Object Explorer
+              Data Based Objects Creation (DDL Commands)
+              Data Manipulation (DML Commands)
+              Accessing Data from Multiple Tables Using SELECT
+              Optimizing Your Work
+
+  
+Module 3.1: SQL Server Reporting Services
+              Basics of SSRS
+              Creating Parameters
+              Understanding Visualization
+              Creating Visualization Using SSRS
+
+  
+Module 3.2: SQL Server Integration Services
+              Understanding Basics of SSIS
+              Understanding Packages
+              Creating Packages to Integrate
+              Creating Project Using SSIS
+
+  
+Module 4: Power BI
+              Introduction
+              Data Preparation and Modeling
+              Data Analysis Expressions (DAX)
+              Reports Development (Visuals in Power BI)
+
+  
+Module 5: Data Analytics Using Python
+              Introduction to Basic Statistics
+              Introduction to Mathematical Foundations
+              Introduction to Analytics & Data Science
+              Python Essentials (Core)
+              Operations with NumPy (Numerical Python)
+              Overview of Pandas
+              Cleansing Data with Python
+              Data Analysis Using Python
+              Data Visualization with Python
+              Statistical Methods & Hypothesis Testing
+
+
+  
+Module 6: Tableau
+              Getting Started
+              Data Handling & Summaries
+              Reports Development (Visuals in Tableau)
+              Module 7: R For Data Science
+              Data Importing/Exporting
+              Data Manipulation
+              Data Analysis
+              Using R with Databases
+              Data Visualization with R
+              Introduction to Statistics
+              Linear Regression: Solving Regression Problems
+
+  
+Module 8: Alteryx
+              Overview of the Alteryx Course and Fundamental Concepts
+              Using the Select Tool to Rename Fields, Change the Data Type
+              Understanding the User Environment and Alteryx Settings
+              Filtering Data/Data Processing
+              Blending/Joining Data from Different Sources
+              Data Cleansing
+              Impute Values
+              Random Sample"
+
+  
